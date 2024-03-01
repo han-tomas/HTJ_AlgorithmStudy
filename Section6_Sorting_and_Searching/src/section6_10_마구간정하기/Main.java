a847a46b59ef66c5e825cd42449c8f441b76fa69
han_taejun <plushan98@naver.com>
2024-03-01 20:10:32 +0900

    day 19

---
+++ Section6_Sorting_and_Searching/src/section6_10_마구간정하기/Main.java
@@ -0,0 +1,48 @@
+package section6_10_마구간정하기;
+import java.util.*;
+
+
+public class Main {
+	public int count(int[] arr, int distance) {
+		int count = 1;
+		int point = arr[0];
+		for (int i = 1; i < arr.length; i ++) {
+			if(arr[i] - point >= distance) {
+				count ++;
+				point = arr[i];
+			}
+		}
+		return count;
+	}
+	
+	
+	public int solution(int n, int c, int[] arr) {
+		int answer = 0;
+		Arrays.sort(arr);
+		int lt = arr[1] - arr[0];
+		int rt = arr[n-1] - arr[0];
+		while (lt <= rt) {
+			int mid = (lt + rt) / 2;
+			if (count(arr , mid) >= c) {
+				answer = mid;
+				lt = mid + 1;
+			}
+			else rt = mid - 1;
+		}
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int c = scan.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		System.out.println(T.solution(n, c, arr));
+	}
+
+}
