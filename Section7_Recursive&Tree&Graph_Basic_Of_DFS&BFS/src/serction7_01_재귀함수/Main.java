6a7da7548eaef5c0a0dd62120d6336b57c0a3c44
han_taejun <plushan98@naver.com>
2024-03-08 22:44:13 +0900

    day 21

---
+++ Section7_Recursive&Tree&Graph_Basic_Of_DFS&BFS/src/serction7_01_재귀함수/Main.java
@@ -0,0 +1,22 @@
+package serction7_01_재귀함수;
+import java.util.*;
+
+
+public class Main {
+	public void solution (int n) {
+		if (n == 0) return;
+		else {
+			solution(n - 1);
+			System.out.print(n + " ");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		T.solution(n);
+	}
+
+}
