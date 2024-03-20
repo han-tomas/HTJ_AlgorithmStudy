6e78630d2787154570375edc8a027058cad785d0
han_taejun <plushan98@naver.com>
2024-03-20 14:50:54 +0900

    day 28

---
+++ Section7_Recursive&Tree&Graph_Basic_Of_DFS&BFS/src/section7_14_그래프최단거리_BFS/Main.java
@@ -0,0 +1,50 @@
+package section7_14_그래프최단거리_BFS;
+import java.util.*;
+
+
+public class Main {
+	static int n, m;
+	static List<ArrayList<Integer>> graph;
+	static int[] ch, dis;
+	
+	public void BFS(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		ch[v] = 1;
+		dis[v] = 0;
+		q.offer(v);
+		while (!q.isEmpty()) {
+			int current = q.poll();
+			for (int x : graph.get(current)) {
+				if(ch[x] == 0) {
+					ch[x] = 1;
+					q.offer(x);
+					dis[x] = dis[current] + 1;
+				}
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		m = scan.nextInt();
+		graph = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		ch = new int[n+1];
+		dis = new int[n+1];
+		for (int i = 0; i < m; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			graph.get(a).add(b);
+		}
+		T.BFS(1);
+		for (int i = 2; i <= n; i++) {
+			System.out.println(i + " : " + dis[i]);
+		}
+	}
+
+}
---
+++ Section7_Recursive&Tree&Graph_Basic_Of_DFS&BFS/src/section7_14_그래프최단거리_BFS/Main2.java
@@ -0,0 +1,51 @@
+package section7_14_그래프최단거리_BFS;
+import java.util.*;
+
+
+public class Main2 {
+	static int n, m;
+	static int[][] graph;
+	static int[] ch, dis;
+	
+	
+	public void BFS(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(v);
+		ch[1] = 1;
+		dis[1] = 0;
+		while(!q.isEmpty()) {
+			int len = q.size();
+			for (int i = 0; i < len; i++) {
+				int current = q.poll();
+				for (int j = 1; j <= n; j++) {
+					if(graph[current][j] == 1 && ch[j] == 0) {
+						ch[j] = 1;
+						dis[j] = dis[current]+ 1;
+						q.offer(j);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Main2 T = new Main2();
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		m = scan.nextInt();
+		ch = new int[n+1];
+		dis = new int[n+1];
+		graph = new int[n+1][n+1];
+		for (int i = 0 ;i < m; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			graph[a][b] = 1;
+		}
+		T.BFS(1);
+		for (int i = 2; i <= n; i++) {
+			System.out.println(i + " : " + dis[i]);
+		}
+	}
+
+}
