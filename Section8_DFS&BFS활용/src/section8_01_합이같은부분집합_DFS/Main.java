1270e420e88e5937031ecdeddccf0be0845f26e3
han_taejun <plushan98@naver.com>
2024-03-21 19:48:04 +0900

    day 30

---
+++ Section8_DFS&BFS활용/src/section8_01_합이같은부분집합_DFS/Main.java
@@ -0,0 +1,40 @@
+package section8_01_합이같은부분집합_DFS;
+import java.util.*;
+
+
+public class Main {
+	static String answer = "NO";
+	static int n, total = 0;
+	boolean flag = false;
+	
+	
+	public void DFS(int L, int sum, int[] arr) {
+		if (flag) return;
+		if (sum > total/2) return;
+		if (L == n) {
+			if ((total - sum) == sum) {
+				answer = "YES";
+				flag = true;
+			}
+		}
+		else {
+			DFS(L+1, sum + arr[L], arr);
+			DFS(L+1, sum, arr);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan= new Scanner(System.in);
+		n = scan.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+			total += arr[i];
+		}
+		T.DFS(0, 0, arr);
+		System.out.println(answer);
+	}
+
+}
