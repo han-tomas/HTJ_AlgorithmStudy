ce7a6eb4cd2f215f4ad8697e332dbfd44728ec12
han_taejun <plushan98@naver.com>
2024-03-26 18:12:15 +0900

    update

--- Section8_DFS&BFS활용/src/section8_07_조합수_메모이제이션/Main2.java
+++ Section8_DFS&BFS활용/src/section8_07_조합수_메모이제이션/Main2.java
@@ -2,17 +2,19 @@ package section8_07_조합수_메모이제이션;
 import java.util.*;
 
 
-public class Main {	
+public class Main2 {	
+	int[][] ch = new int[35][35];
 	public int DFS(int n, int r) {
+		if (ch[n][r] > 0) return ch[n][r];
 		if (n == r || r == 0) {
 			return 1;
 		}
-		else return DFS(n-1, r-1) + DFS(n-1, r);
+		else return ch[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
 	}
 	
 	
 	public static void main(String[] args) {
-		Main T = new Main();
+		Main2 T = new Main2();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int r = scan.nextInt();
