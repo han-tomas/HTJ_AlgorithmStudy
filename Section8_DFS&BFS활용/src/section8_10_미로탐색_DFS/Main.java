623153c0de91f8002856421944598214772cabd4
han_taejun <plushan98@naver.com>
2024-03-29 17:50:44 +0900

    day 37

---
+++ Section8_DFS&BFS활용/src/section8_10_미로탐색_DFS/Main.java
@@ -0,0 +1,44 @@
+package section8_10_미로탐색_DFS;
+import java.util.*;
+
+
+public class Main {
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	static int[][] miro;
+	static int answer = 0;
+	
+	
+	public void DFS(int x, int y) {
+		if(x == 7 && y == 7) {
+			answer ++;
+		}
+		else {
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && miro[nx][ny] == 0) {
+					miro[nx][ny] = 1;
+					DFS(nx, ny);
+					miro[nx][ny] = 0;
+				}
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan = new Scanner(System.in);
+		miro = new int[8][8];
+		for (int i = 1; i <= 7; i ++) {
+			for (int j = 1; j <=7; j++) {				
+				miro[i][j] = scan.nextInt();
+			}
+		}
+		miro[1][1] = 1;
+		T.DFS(1, 1);
+		System.out.println(answer);
+	}
+
+}
